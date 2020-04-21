f86bb75cdd7e8488a092e778a9225a641b95a0fe
Marco Ramos <mfilipe@gmail.com>
2020-04-21 17:49:14 -0400

    answers to questions

--- js/quiz.js
+++ js/quiz.js
@@ -1,8 +1,15 @@
+let correctAnswers = 0;
+
 /* 
   1. Store correct answers
-   - When quiz begins, no answers are correct
+  - When quiz begins, no answers are correct
 */
 
+const correctAnswer1 = 'Washington, D.C.';
+const correctAnswer2 = ['three', '3'];
+const correctAnswer3 = 1974;
+const correctAnswer4 = 'Luanda';
+const correctAnswer5 = ['3days', '3', 'three', 'three days'];
 
 // 2. Store the rank of a player
 
@@ -12,25 +19,25 @@
 
 /*
   4. Ask at least 5 questions
-   - Store each answer in a variable
-   - Keep track of the number of correct answers
+  - Store each answer in a variable
+  - Keep track of the number of correct answers
 */
 
 /*
 - Questions to use.
 - What is the capital of the United States?
 - How many colors in the Colombian flag?
-- WHat was the year Portugal was free from dictatorship?
+- What was the year Portugal was free from dictatorship?
 - What is the capital of Angola?
 - How many days does it take to get to the moon?
 */
 
 /*
   5. Rank player based on number of correct answers
-   - 5 correct = Gold
-   - 3-4 correct = Silver
-   - 1-2 correct = Bronze
-   - 0 correct = No crown
+  - 5 correct = Gold
+  - 3-4 correct = Silver
+  - 1-2 correct = Bronze
+  - 0 correct = No crown
 */
 
 
