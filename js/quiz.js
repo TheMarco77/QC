21b26a33a04097bcec2969e1b5772a66120bae75
Marco Ramos <mfilipe@gmail.com>
2020-04-24 18:55:40 -0400

    cleanedUpCode

--- js/quiz.js
+++ js/quiz.js
@@ -1,18 +1,25 @@
-let correctAnswers = 0;
+/*
+1. Store correct answers
+- when quiz begins, no answers are correct  
+*/
+let correct = 0;
 
-const correctOne = 'DC';
-const correctTwo = '3';
-const correctThree = '1974';
-const correctFour = 'Luanda';
-const correctFive = '3';
+// const correctOne = 'DC';
+// const correctTwo = '3';
+// const correctThree = '1974';
+// const correctFour = 'Luanda';
+// const correctFive = '3';
 
 // 2. Store the rank of a player
-// const rankOne = 'Gold';
+let rank;
+
+// const rankOne = 'You earned the Gold Crown ';
 // const rankTwo = 'Silver';
 // const rankThree = 'Bronze';
 // const noRank = 'Sorry, you did not rank!';
 
 // 3. Select the <main> HTML element
+const main = document.querySelector('main');
 
 /*
   4. Ask at least 5 questions
@@ -20,33 +27,32 @@ const correctFive = '3';
   - Keep track of the number of correct answers
 */
 
-const qOne = prompt('What is the capital of the United States?');
-const qTwo = prompt('How many colors in the Colombian flag?');
-const qThree = prompt('What was the year Portugal was free from dictatorship?');
-const qFour = prompt('What is the capital of Angola, Africa?');
-const qFive = prompt('How many days does it take to get to the moon?');
+const Q1 = prompt('What is the capital of the United States?');
+if (Q1.toUpperCase() === 'DC') {
+  correct++;
+}
 
-if (qOne.toUpperCase() === correctOne) {
-  correctAnswers++;
-  console.log(correctAnswers);
+const Q2 = prompt('How many colors in the Colombian flag?');
+if (Q2 === '3') {
+  correct++;
 }
-if (qTwo === correctTwo) {
-  correctAnswers++;
-  console.log(correctAnswers);
+
+const Q3 = prompt('What was the year Portugal was free from dictatorship?');
+if (Q3 === '1974') {
+  correct++;
 }
-if (qThree === correctThree) {
-  correctAnswers++;
-  console.log(correctAnswers);
+
+const Q4 = prompt('What is the capital of Angola, Africa?');
+if (Q4[0].toUpperCase() + Q4.slice(1).toLowerCase() === 'Luanda') {
+  correct++;
 }
-if (qFour[0].toUpperCase() + qFour.slice(1) === correctFour) {
-  correctAnswers++;
-  console.log(correctAnswers);
+
+const Q5 = prompt('How many days does it take to get to the moon?');
+if (Q5 === '3') {
+  correct++;
 }
-if (qFive === correctFive) {
-  correctAnswers++;
-  console.log(correctAnswers);
-}
-console.log(correctAnswers);
+
+// console.log(correctAnswers);
 /*
   5. Rank player based on number of correct answers
   - 5 correct = Gold
@@ -54,16 +60,20 @@ console.log(correctAnswers);
   - 1-2 correct = Bronze
   - 0 correct = No crown
 */
-if (correctAnswers === 5) {
-  console.log('You got all correct, you won the GOLD CROWN');
-} else if (correctAnswers === 4 || correctAnswers === 3) {
-  console.log('You earned the SILVER CROWN');
-} else if (correctAnswers === 2 || correctAnswers === 1) {
-  console.log('You earned the BRONZE CROWN');
+
+if (correct === 5) {
+  rank = 'GOLD';
+} else if (correct >= 3) {
+  rank = 'SILVER';
+} else if (correct >= 2) {
+  rank = 'BRONZE';
 } else {
-  console.log('Sorry you got the all wrong!');
+  rank = 'None :(';
 }
+
 // 6. Output results to the <main> element
 document.querySelector(
   'main'
-).innerHTML = `<h1 style="text-align:center">You got ${correctAnswers} out of 5 questions correct.</h1>`;
+).innerHTML = `<h2 style="text-align:center">You got ${correct} out of 5 questions correct.</h2>
+<p style="text-align:center">Crown earned: <strong>${rank}</strong><p>
+`;
